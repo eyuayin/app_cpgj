f40b8cf6130b816398356c80d289799182909499
brown.yuan <brown.yuan@trantect.com>
2016-06-12 23:59:48 +0800

    weixin selected classes part

--- member_selected_classes_weixin.php
+++ member_selected_classes_weixin.php
@@ -16,39 +16,57 @@ print <<<EOT
 <body style="margin:0">
   <!-- Table markup-->  
   <div class="div-title" style="line-height:40px;">我的课程</div>
-  
-  <div class="div-class-header">week1 / month1 date1</div>
-  <table id="day1" cellpadding="6" cellspacing="0" width="100%">  
-    <tbody>
-      <volist name="data1" id="vo1">
-        <tr style="height: 60px;" onclick="cancel_class(this);" id="class_id">
-          <td class="class-status"><input value=2></td>
-          <td class="hide-input"><input value="canceled"></td>
-          <td class="td-class-time-valid">class_begin_time</td>  
-          <td class="td-class-info-valid"><div>class_name</div><div class="inner-small">teacher_name</div></td>
-          <td class="td-right-valid1"><span>your_status</span></td>
-          <td class="td-right-valid2"><img src="wx_image/right-arrow.jpg" style="height: 40%;"></td>
-        </tr>
-      </volist>
-    </tbody>
-  </table>
 
-  <div style="height:10px;"></div>
-  <div class="div-class-header">week2 / month2 date2</div>
-  <table id="day2" cellpadding="6" cellspacing="0" width="100%">  
-    <tbody>
-      <volist name="data2" id="vo2">
-        <tr style="height: 60px;" onclick="cancel_class(this);" id="vo2class_id">
-          <td class="class-status"><input value=2></td>
-          <td class="hide-input"><input value="canceled"></td>
-          <td class="td-class-time-valid">class_begin_time</td>  
-          <td class="td-class-info-valid"><div>class_name</div><div class="inner-small">teacher_name</div></td>
-          <td class="td-right-valid1"><span>your_status</span></td>
-          <td class="td-right-valid2"><img src="wx_image/right-arrow.jpg" style="height: 40%;"></td>
-        </tr>
-      </volist>
-    </tbody>
-  </table>
+EOT;
+
+require("./constant_var_define.php");
+
+    $input_open_id = $_GET['openid'];
+    debug_output("输入的open_id是：".$input_open_id);
+
+    if(empty($input_open_id))
+    {
+        page_output("black", 15, "未获取微信ID，请联系管理员！");
+        //echo "此微信号尚未绑定！";
+        return;
+    }
+
+    //连接数据库的参数
+    $conn = db_connect();
+    
+    //根据open_id查出card_id,member_id和member_name
+    $query_result = $conn->query("select * from member_booked_class_view where open_id='".$input_open_id."'");
+    debug_output("select * from member_booked_class_view where open_id='".$input_open_id."'");
+    $result = $query_result->fetchAll();
+    if(empty($result))
+    {
+        page_output("black", 15, "此微信号尚未绑定，无法查询！");
+        //echo "此微信号尚未绑定，无法查询！";
+        return;
+    }
+    
+    //循环将所有输入的会员名字的课程列出来
+    foreach($result as $value)  //member_info_table 0:member_id 1:card_id 2:member_name
+    {
+          print <<<EOT
+          <div class="div-class-header"> / month1 date1</div>
+          <table id="day1" cellpadding="6" cellspacing="0" width="100%">  
+            <tbody>
+              <volist name="data1" id="vo1">
+                <tr style="height: 60px;" onclick="cancel_class(this);" id="class_id">
+                  <td class="class-status"><input value=2></td>
+                  <td class="hide-input"><input value="canceled"></td>
+                  <td class="td-class-time-valid">class_begin_time</td>  
+                  <td class="td-class-info-valid"><div>class_name</div><div class="inner-small">teacher_name</div></td>
+                  <td class="td-right-valid1"><span>your_status</span></td>
+                  <td class="td-right-valid2"><img src="wx_image/right-arrow.jpg" style="height: 40%;"></td>
+                </tr>
+              </volist>
+            </tbody>
+          </table>
+EOT;
+    }
+
 
   
 <script type="text/javascript" src="FE_all_cui/js/jquery.min.js"></script> 
