0b5327739750f590e797dc71ef8e4ea2284611dc
brown.yuan <brown.yuan@trantect.com>
2016-06-13 23:48:09 +0800

    select classes weixin is ready, click is not ok

--- member_selected_classes_weixin.php
+++ member_selected_classes_weixin.php
@@ -21,9 +21,15 @@ EOT;
 
 require("./constant_var_define.php");
 
-    $input_open_id = $_GET['openid'];
-    debug_output("输入的open_id是：".$input_open_id);
+    
+   
+    
+    //$input_open_id = $_GET['openid'];
+    
 
+    $input_open_id = "oqUOZwUXs9YeUF0uMyWr9-M8cH3B";
+    debug_output("输入的open_id是：".$input_open_id);
+    
     if(empty($input_open_id))
     {
         page_output("black", 15, "未获取微信ID，请联系管理员！");
@@ -33,10 +39,11 @@ require("./constant_var_define.php");
 
     //连接数据库的参数
     $conn = db_connect();
+    $today =  date('y-m-d',time());
     
     //根据open_id查出card_id,member_id和member_name
-    $query_result = $conn->query("select * from member_booked_class_view where open_id='".$input_open_id."'");
-    debug_output("select * from member_booked_class_view where open_id='".$input_open_id."'");
+    $query_result = $conn->query("select * from member_booked_class_detail_view where open_id='".$input_open_id."' ORDER BY date desc");
+    debug_output("select * from member_booked_class_detail_view where open_id='".$input_open_id."' ORDER BY date desc");
     $result = $query_result->fetchAll();
     if(empty($result))
     {
@@ -45,20 +52,57 @@ require("./constant_var_define.php");
         return;
     }
     
+     
+    
     //循环将所有输入的会员名字的课程列出来
     foreach($result as $value)  //member_info_table 0:member_id 1:card_id 2:member_name
     {
+         switch ($value[9])
+                {
+                case 1:
+                  $value[9] = "星期 日 ";
+                  break;
+                case 2:
+                  $value[9] =  " 星期 一 ";
+                  break;
+                case 3:
+                  $value[9] =  " 星期 二 ";
+                  break;
+                case 4:
+                  $value[9] =  " 星期 三 ";
+                  break;
+                case 5:
+                  $value[9] =  " 星期 四 ";
+                  break;
+                case 6:
+                  $value[9] =  " 星期 五 ";
+                  break;
+                case 7:
+                  $value[9] =  " 星期 六 ";
+                  break;
+                }
+                
+         switch ($value[2])
+                {
+                case 0:
+                  $value[2] = "已预约";
+                  break;
+                case 1:
+                  $value[2] =  "已取消";
+                  break;
+                }
+                
           print <<<EOT
-          <div class="div-class-header"> / month1 date1</div>
+          <div class="div-class-header"> {$value[9]} / {$value[8]} </div>
           <table id="day1" cellpadding="6" cellspacing="0" width="100%">  
             <tbody>
               <volist name="data1" id="vo1">
                 <tr style="height: 60px;" onclick="cancel_class(this);" id="class_id">
-                  <td class="class-status"><input value=2></td>
+                  <td class="class-status"><input value="{$value[1]}"></td>
                   <td class="hide-input"><input value="canceled"></td>
-                  <td class="td-class-time-valid">class_begin_time</td>  
-                  <td class="td-class-info-valid"><div>class_name</div><div class="inner-small">teacher_name</div></td>
-                  <td class="td-right-valid1"><span>your_status</span></td>
+                  <td class="td-class-time-valid">{$value[5]}</td>  
+                  <td class="td-class-info-valid"><div>{$value[6]}</div><div class="inner-small">{$value[7]}</div></td>
+                  <td class="td-right-valid1"><span>{$value[2]}</span></td>
                   <td class="td-right-valid2"><img src="wx_image/right-arrow.jpg" style="height: 40%;"></td>
                 </tr>
               </volist>
@@ -67,7 +111,7 @@ require("./constant_var_define.php");
 EOT;
     }
 
-
+/*
   
 <script type="text/javascript" src="FE_all_cui/js/jquery.min.js"></script> 
 <script>
@@ -138,5 +182,5 @@ function cancel_class(obj){
 </html>
 
 EOT;
-
+*/
 ?>
