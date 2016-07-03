d8fd7fb993cf0b45dca9b6f6c8e1f066cc303cbd
brown.yuan <brown.yuan@trantect.com>
2016-07-03 14:52:13 +0800

    change footer

--- FE_all_cui/page_footer.php
+++ FE_all_cui/page_footer.php
@@ -1,7 +1,7 @@
 <?php
 print <<<EOT
 <footer class="well">
-    <a target="_blank" title="XX瑜伽">XX瑜伽</a> - 网站制作：<a href="#" title="南京沃卓" target="_blank">南京沃卓</a>
+    <a target="_blank" title="天环瑜伽">天环瑜伽</a>
 </footer>
 EOT;
 ?>
