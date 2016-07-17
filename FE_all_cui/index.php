52a1e4510ba053c4c4362e945d55c24f20ec0890
brown.yuan <brown.yuan@trantect.com>
2016-07-17 23:51:57 +0800

    inde is ok,index.php navigator and search_class_cui

--- FE_all_cui/index.php
+++ FE_all_cui/index.php
@@ -1,56 +1,83 @@
-<?php
-session_start();
-if (isset($_SESSION['valid_user'])){
-print <<<EOT
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Admin | Strass</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Admin panel developed with the Bootstrap from Twitter.">
-    <meta name="author" content="travis">
-
-    <link href="css/bootstrap.css" rel="stylesheet">
-	<link href="css/site.css" rel="stylesheet">
-    <link href="css/bootstrap-responsive.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-EOT;
-    include("./navigator_bar.php");
-print <<<EOT
+    <head>
+        <meta http-equiv="Cache-Control" content="no-cache" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link href="css/bootstrap.css" rel="stylesheet"></link>
+        <link href="css/site.css" rel="stylesheet"></link>
+        <link href="css/bootstrap-responsive.css" rel="stylesheet"></link>
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
     <div class="container-fluid">
       <div class="row-fluid">
         <div class="span9">
           <div class="well hero-unit">
-            <h1>欢迎, 管理员</h1>
-            <p>欢迎访问本会员管理系统 </p>
-            <p><a class="btn btn-success btn-large" href="new-user.php">新建会员</a></p>
-            <p><a class="btn btn-success btn-large" href="search_and_update_user.php">查找会员</a></p>
-              <p><a class="btn btn-success btn-large" href="new-role.php">预约课程</a></p>
-              <p><a class="btn btn-success btn-large" href="search_and_update_class.php">查看预约情况</a></p>
+            <div style="float:left"><a class="btn btn-success btn-large" href="new-user.php">新建会员</a></div>
+            <div style="float:left; margin-left: 10px;"><a class="btn btn-success btn-large" href="search_and_update_user.php">查找会员</a></div>
+              <div style="float:left;margin-left: 10px;"><a class="btn btn-success btn-large" href="new-role.php">预约课程</a></div>
+              <div style="float:left;margin-left: 10px;"><a class="btn btn-success btn-large" href="search_and_update_class.php">查看预约情况</a></div>
           </div>
 		  <br />
         </div>
       </div>
-
       <hr>
-
-
     </div>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span9">
+                    <div class="row-fluid">
+                        <div class="page-header">
+                            <h1>Selected class <small>当日课程列表</small></h1>
 
-    <script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+                        </div>
+                        <table class="table table-striped table-bordered table-condensed">
+                            <thead>
+                                <tr>
+                                    <th>姓名</th>
+                                    <th>会员卡号</th>
+                                    <th>电话号码</th>
+                                    <th>课程编号</th>
+                                    <th>课程名称</th>
+                                    <th>上课时间</th>
+                                </tr>
+                            </thead>
+<?php
+    include("./navigator_bar.php");
+    require("../constant_var_define.php");
+    $conn = db_connect();
+
+    //根据open_id查出card_id,member_id
+    $query_result = $conn->query("select * from book_info_for_today_cui");
+    $result = $query_result->fetchAll();
+    
+    //循环将所有输入的会员名字的课程列出来
+    foreach($result as $value)
+    {
+        
+print <<<EOT
+                    <tbody>
+                    <tr>
+                    <td >{$value[3]}</td> 
+                    <td >{$value[2]}</td>
+                    <td >{$value[4]}</td>
+                    <td >{$value[0]}</td>
+                    <td >{$value[5]}</td>
+                    <td >{$value[1]}</td
+                    </tr>
+ 
 EOT;
-    include("./page_footer.php");
-}
-else{
-    echo "please login first!";
-    echo "<a href='login.html'>login</a>";
-}
+              
+               }
+               
+
 ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
